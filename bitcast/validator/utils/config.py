c71c6a841725346759ac531a18df42f89c3718c0
mizu-tx <willblears.wb@gmail.com>
2025-05-15 16:44:03 +0000

    update version

--- bitcast/validator/utils/config.py
+++ bitcast/validator/utils/config.py
@@ -16,7 +16,7 @@ CACHE_DIRS = {
     "blacklist": os.path.join(CACHE_ROOT, "blacklist")
 }
 
-__version__ = "1.2.2"
+__version__ = "1.2.3"
 
 # required
 BITCAST_SERVER_URL = os.getenv('BITCAST_SERVER_URL', 'http://44.227.253.127')
